7f1070136272dfa2baec5133a44410cb4de31eb2
hawk198723 <hawk198723@gmail.com>
2022-10-18 09:29:46 -0700

    refresh Threads

---
+++ src/Threads/RunningThreads.java
@@ -0,0 +1,12 @@
+package Threads;
+
+public class RunningThreads extends Thread {
+    public static void main(String[] args) {
+        RunningThreads thread = new RunningThreads();
+        thread.start();
+        System.out.println("This code is outside of the thread");
+    }
+    public void run() {
+        System.out.println("This code is running in a thread");
+    }
+}
