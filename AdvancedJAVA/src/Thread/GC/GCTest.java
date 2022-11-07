f66f0131a61104518e7d843ff1f2da3c38e076a8
hawk198723 <hawk198723@gmail.com>
2022-11-07 12:35:42 -0800

    GC

--- AdvancedJAVA/src/Thread/GC/GCTest.java
+++ AdvancedJAVA/src/Thread/GC/GCTest.java
@@ -1,6 +1,4 @@
 package Thread.GC;
-
-
 public class GCTest{
     public int x = 0;
     public int value = 0;
@@ -8,28 +6,35 @@ public class GCTest{
         System.out.println("Hello");
     }
 
-    public Object Lego(){
-        this.value = 10;
+    public static Object Lego(){
         int x = 0;
-
-        Runtime runtime = Runtime.getRuntime();
-        runtime.gc();
-
+        System.out.println(Runtime.getRuntime().totalMemory());
+//        Runtime runtime = Runtime.getRuntime();
+//        runtime.gc();
+        Runtime.getRuntime().gc();
+        System.out.println(Runtime.getRuntime().totalMemory());
+        System.out.println(Runtime.getRuntime().freeMemory());
         System.out.println("Building");
         return x;
     }
 
     public static void main(String[] args) {
 
-        GCTest obj=new GCTest();
-        obj=null;
+        GCTest obj = new GCTest();
+        obj = null;
 
         GCTest a = new GCTest();
         GCTest b = new GCTest();
         b = a;
 
+        Lego();
         System.out.println("Collecting garbage...");
+
+//        System.out.println(Runtime.getRuntime().totalMemory());
 //        System.gc();
+//        System.out.println(Runtime.getRuntime().totalMemory());
+//        System.out.println(Runtime.getRuntime().freeMemory());
+
 
         System.out.println("Done...");
     }
