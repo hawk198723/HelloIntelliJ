b5328d6d9395a805105036392099d93fab1db69b
hawk198723 <hawk198723@gmail.com>
2022-11-25 19:09:41 -0800

    Java 1.8

--- AdvancedJAVA/src/JAVA8/StreamAPI/exe1/StreamAPITest.java
+++ AdvancedJAVA/src/JAVA8/StreamAPI/exe1/StreamAPITest.java
@@ -1,4 +1,15 @@
 package JAVA8.StreamAPI.exe1;
 
 public class StreamAPITest {
+
+//    @Test
+    public static void main(String args[]) {
+        int x, y;
+
+        x = 5 >> 2;
+        y = x >>> 2;
+        System.out.println(y);
+    }
+
+
 }
