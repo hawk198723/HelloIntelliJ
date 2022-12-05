a054304c5f40799e75107b037588b812361e123d
hawk198723 <hawk198723@gmail.com>
2022-12-05 13:40:54 -0800

    methodref

--- AdvancedJAVA/src/JAVA8/ConstructorReference/exe1/ConstructorRef.java
+++ AdvancedJAVA/src/JAVA8/ConstructorReference/exe1/ConstructorRef.java
@@ -3,17 +3,18 @@ package JAVA8.ConstructorReference.exe1;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class ConstructorRef {
     @Test
     public void test1(){
-//        BiFunction<Integer, String, Employee> function = (id, name) -> new Employee(id, name);
-//
-//        System.out.println(function.apply(1001, "Jason"));
-//
-//        BiFunction<Integer,String, Employee> function1 = Employee :: new;
-//        System.out.println(function1.apply(1000, "Kim"));
+        BiFunction<Integer, String, Employee> function = (id, name) -> new Employee(id, name);
+
+        System.out.println(function.apply(1001, "Jason"));
+
+        BiFunction<Integer,String, Employee> function1 = Employee :: new;
+        System.out.println(function1.apply(1000, "Kim"));
     }
     @Test
     public void test2(){
