190e3b5f8ba9a3ac3da280067735fa84b90279c4
hawk198723 <hawk198723@gmail.com>
2022-12-11 17:40:33 -0800

    test

---
+++ src/BasicTest/ClassA.java
@@ -0,0 +1,34 @@
+package BasicTest;
+
+public class ClassA {
+
+
+    public ClassA() {}
+
+    public void print() {
+        System.out.println("Im Class A");
+    }
+}
+
+class ClassB {
+
+//    public static void main(String[] args) {
+//        ClassA a = new ClassA();
+//        a.print();
+//    }
+    public ClassB() {}
+
+    public void print() {
+        System.out.println("Class B");
+    }
+}
+
+//class Main {
+//    public static void main(String[] args) {
+//        ClassA classA = new ClassA();
+//        classA.print();
+//
+//        ClassB classB = new ClassB();
+//        classB.print();
+//    }
+//}
---
+++ src/BasicTest/Main.java
@@ -0,0 +1,11 @@
+package BasicTest;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassA classA = new ClassA();
+        classA.print();
+
+        ClassB classB = new ClassB();
+        classB.print();
+    }
+}
